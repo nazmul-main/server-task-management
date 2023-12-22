46750fe645cd9193a3efe66dbaf90accfebf7fb7
nazmulhossen1 <nazmulhossen.main@gmail.com>
2023-12-22 10:39:13 +0600

    added todo, ongoing, complete route and hard coded main

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.idkvt6k.mongodb.net/?retryWrites=true&w=majority`;
 
+const TaskManegement = 'smooth_taks';
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -26,10 +28,19 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.connect();
+
+    const To_do_Collection = client.db(TaskManegement).collection("to_do_task");
+
+    //task added api 
+    app.post('/task' , async (req, res) => {
+      console.log(req.body);
+      res.send
+    })
+
+    
+    
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
